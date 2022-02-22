fd7ac499f0e991c2cd5ab875c40fb5ebbac16acd
stephannavarro <stephan.navarro@fr.ibm.com>
2022-02-22 16:37:35 +0100

    adding tower playbook

--- roles/tower_user_reset/files/chansiblewas_password.sh
+++ roles/tower_user_reset/files/chansiblewas_password.sh
@@ -1,4 +1,4 @@
 #cd /var/lib/awx/scripts
 echo $(pwd)
 echo $(ls)
-#ansible-playbook tower.yml --extra-vars "user_name=$1 user_password=$2"
+ansible-playbook scripts/tower.yml --extra-vars "user_name=$1 user_password=$2"
